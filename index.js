0265c3f1283811b2b50710748ccff10a1097b888
arparvej1 <parvej8pro@gmail.com>
2024-07-16 15:35:16 +0600

    add netlify link

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://pcash-client.vercel.app'
+    'https://pcash-client.vercel.app',
+    'https://pcash.netlify.app'
   ],
   credentials: true
 }));
